34ab2b65187b05aa78f64b12a83154d24bb34e8c
MohabHelmi <mohab.helmi@bcestudent.com>
2025-02-07 11:25:46 +0000

    done

--- task_file.py
+++ task_file.py
@@ -1,10 +1,17 @@
 print("Hello!") 
 
-list = []
+list = ["make a cake", "clean the house", "go to the gym", "do the laundry", "go to the store"]
 
+print(list)
 
 add = input(print("What is todays list? "))
 
 list.append(add)
+
+print(list)
+
+remove = input(print("What have you done? "))
+
+list.remove(remove)
 
 print(list)
